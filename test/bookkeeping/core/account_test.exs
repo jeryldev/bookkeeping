15932ee2beff9d655a80b8b0d455007507190e42
Adrian Estopace <estopaceadrian@gmail.com>
2023-08-16 23:35:22 +0800

    [B-5] Add account core and test (#11)
    [B-5] Add account core and test (#11)

---
+++ test/bookkeeping/core/account_test.exs
@@ -0,0 +1,45 @@
+defmodule Bookkeeping.Core.AccountTest do
+  use ExUnit.Case, async: true
+  alias Bookkeeping.Core.{Account, AccountType, EntryType, ReportingCategory}
+
+  test "allow integer code, binary name and account type account field" do
+    {:ok, account_type} = AccountType.asset()
+    new_account = Account.new(10000, "cash", account_type)
+
+    assert ^new_account =
+             {:ok,
+              %Account{
+                code: 10000,
+                name: "cash",
+                account_type: %AccountType{
+                  name: "Asset",
+                  normal_balance: %EntryType{type: :debit, name: "Debit"},
+                  primary_reporting_category: %ReportingCategory{
+                    type: :balance_sheet,
+                    primary: true
+                  },
+                  contra: false
+                }
+              }}
+  end
+
+  test "disallow non-integer code field" do
+    {:ok, account_type} = AccountType.asset()
+    new_account = Account.new("10000", "cash", account_type)
+
+    assert ^new_account = {:error, :invalid_account}
+  end
+
+  test "disallow non-binary name field" do
+    {:ok, account_type} = AccountType.asset()
+    new_account = Account.new(10000, 10000, account_type)
+
+    assert ^new_account = {:error, :invalid_account}
+  end
+
+  test "disallow non-%AccountType{} account field" do
+    new_account = Account.new(10000, "cash", "account_type")
+
+    assert ^new_account = {:error, :invalid_account}
+  end
+end
