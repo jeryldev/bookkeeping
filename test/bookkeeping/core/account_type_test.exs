3c24b63b90c603ea0594f72d7cad9cbd15bc30dc
jeryldev <jeryldev@gmail.com>
2023-08-17 22:31:32 +0800

    add select account type test

--- test/bookkeeping/core/account_type_test.exs
+++ test/bookkeeping/core/account_type_test.exs
@@ -2,6 +2,200 @@ defmodule Bookkeeping.Core.AccountTypeTest do
   use ExUnit.Case, async: true
   alias Bookkeeping.Core.{AccountType, EntryType, ReportingCategory}
 
+  test "select asset account type" do
+    assert AccountType.select_account_type("asset") ==
+             {:ok,
+              %AccountType{
+                name: "Asset",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :balance_sheet,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select liability account type" do
+    assert AccountType.select_account_type("liability") ==
+             {:ok,
+              %AccountType{
+                name: "Liability",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :balance_sheet,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select equity account type" do
+    assert AccountType.select_account_type("equity") ==
+             {:ok,
+              %AccountType{
+                name: "Equity",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :balance_sheet,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select expense account type" do
+    assert AccountType.select_account_type("expense") ==
+             {:ok,
+              %AccountType{
+                name: "Expense",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select revenue account type" do
+    assert AccountType.select_account_type("revenue") ==
+             {:ok,
+              %AccountType{
+                name: "Revenue",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select loss account type" do
+    assert AccountType.select_account_type("loss") ==
+             {:ok,
+              %AccountType{
+                name: "Loss",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select gain account type" do
+    assert AccountType.select_account_type("gain") ==
+             {:ok,
+              %AccountType{
+                name: "Gain",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                }
+              }}
+  end
+
+  test "select contra asset account type" do
+    assert AccountType.select_account_type("contra_asset") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Asset",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :balance_sheet,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "select contra liability account type" do
+    assert AccountType.select_account_type("contra_liability") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Liability",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :balance_sheet,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "select contra equity account type" do
+    assert AccountType.select_account_type("contra_equity") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Equity",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :balance_sheet,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "select contra expense account type" do
+    assert AccountType.select_account_type("contra_expense") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Expense",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "select contra revenue account type" do
+    assert AccountType.select_account_type("contra_revenue") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Revenue",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "select contra gain account type" do
+    assert AccountType.select_account_type("contra_gain") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Gain",
+                normal_balance: %EntryType{type: :debit, name: "Debit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "select contra loss account type" do
+    assert AccountType.select_account_type("contra_loss") ==
+             {:ok,
+              %AccountType{
+                name: "Contra Loss",
+                normal_balance: %EntryType{type: :credit, name: "Credit"},
+                primary_reporting_category: %ReportingCategory{
+                  type: :profit_and_loss,
+                  primary: true
+                },
+                contra: true
+              }}
+  end
+
+  test "disallow account type that is not in the list" do
+    assert AccountType.select_account_type("invalid") ==
+             {:error, :invalid_account_type}
+  end
+
   test "create Asset account type" do
     assert AccountType.asset() ==
              {:ok,
