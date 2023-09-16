6b06e5710ad3353f811562ea10de3e8d3695d51d
jeryldev <jeryldev@gmail.com>
2023-09-16 09:43:32 +0800

    add function specs

--- lib/bookkeeping/boundary/accounting_journal/supervisor.ex
+++ lib/bookkeeping/boundary/accounting_journal/supervisor.ex
@@ -8,13 +8,18 @@ defmodule Bookkeeping.Boundary.AccountingJournal.Supervisor do
   alias Bookkeeping.Boundary.AccountingJournal.Backup, as: AccountingJournalBackup
   alias Bookkeeping.Boundary.AccountingJournal.Server, as: AccountingJournalServer
 
-  @spec start_link([{:name, atom | {:global, any} | {:via, atom, any}}]) ::
-          :ignore | {:error, any} | {:ok, pid}
+  @type init_options_t :: list()
+  @type sup_flags_t :: map()
+  @type children_specs_t :: list(:supervisor.child_spec())
+
+  @spec start_link(init_options_t()) ::
+          {:ok, pid} | {:error, {:already_started, pid()} | {:shutdown, term()} | term()}
   def start_link(options \\ []) do
     Supervisor.start_link(__MODULE__, :ok, options)
   end
 
   @impl true
+  @spec init(any()) :: {:ok, {sup_flags_t(), children_specs_t}}
   def init(_init_arg) do
     children = [
       {AccountingJournalBackup, %{}},
--- lib/bookkeeping/boundary/chart_of_accounts/supervisor.ex
+++ lib/bookkeeping/boundary/chart_of_accounts/supervisor.ex
@@ -8,11 +8,18 @@ defmodule Bookkeeping.Boundary.ChartOfAccounts.Supervisor do
   alias Bookkeeping.Boundary.ChartOfAccounts.Backup, as: ChartOfAccountsBackup
   alias Bookkeeping.Boundary.ChartOfAccounts.Server, as: ChartOfAccountsServer
 
+  @type init_options_t :: list()
+  @type sup_flags_t :: map()
+  @type children_specs_t :: list(:supervisor.child_spec())
+
+  @spec start_link(init_options_t()) ::
+          {:ok, pid} | {:error, {:already_started, pid()} | {:shutdown, term()} | term()}
   def start_link(options \\ []) do
     Supervisor.start_link(__MODULE__, :ok, options)
   end
 
   @impl true
+  @spec init(any()) :: {:ok, {sup_flags_t(), children_specs_t}}
   def init(_init_arg) do
     children = [
       {ChartOfAccountsBackup, %{}},
